d3e89b010b00f1475131ad6c0973d03e01c41455
Mohammadjavad Valipoor <mv@softtelecom.eu>
2015-02-27 12:56:55 +0100

    - adding script to get contents on player from ICN service

--- dss-side-scripts/icn_getcontents.py
+++ dss-side-scripts/icn_getcontents.py
@@ -2,3 +2,110 @@
 # -*- coding: utf-8 -*-
 __author__ = "Mohammad Valipoor"
 __copyright__ = "Copyright 2014, SoftTelecom"
+
+import json
+import logging
+import time
+import httplib2 as http
+from subprocess import call
+import sys
+
+def config_logger(log_level=logging.DEBUG):
+    logging.basicConfig(format='%(levelname)s %(asctime)s: %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p',
+                        log_level=log_level)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(log_level)
+
+    hdlr = logging.FileHandler('icn_getcontents_log.txt')
+    formatter = logging.Formatter(fmt='%(levelname)s %(asctime)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+
+    return logger
+
+LOG = config_logger()
+
+class IcnContentManager:
+
+    def doRequest(self, target_url, req_type, json_data):
+        response_status = 0
+        while (response_status < 200 or response_status >= 400):
+            time.sleep(1)
+            headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+            try:
+                h = http.Http()
+                h.timeout = 30
+
+                LOG.debug("Sending request to:" + target_url)
+                response, content = h.request(target_url, req_type, json_data, headers)
+            except Exception as e:
+                LOG.debug("Handled " + target_url + " exception." + str(e))
+                continue
+            response_status = int(response.get("status"))
+            LOG.debug("response status is:" + str(response_status) + " Content: " + str(content))
+            if (response_status < 200 or response_status >= 400):
+                continue
+            content_dict = json.loads(content)
+            return content_dict
+
+    def generate_contentlist(self, data):
+        contentlist = []
+        if data is not None:
+            for item in data.get("contents"):
+                contentlist.append(item.get("filename"))
+        return contentlist
+
+    def get_file_from_icn(self, filename, prefix, http_server_path):
+        ret_code = -1
+        if filename != "" and prefix != "":
+            LOG.debug("Running command: " + '/home/ubuntu/ccnxdir/bin/ccngetfile ' + '-v ' + 'ccnx:' + prefix + '/' + filename + ' ' + http_server_path + filename)
+            ret_code = call(['/home/ubuntu/ccnxdir/bin/ccngetfile', '-v', 'ccnx:/' + prefix + '/' + filename, http_server_path + filename])
+            ret_code = 0
+        return ret_code
+
+if __name__ == "__main__":
+    total = len(sys.argv)
+    if (total < 2):
+        print ("Usage: python icn_getcontents.py <URL_TO_POLL_FROM> [<HTTP_SERVER_PATH> default: /var/www/] [<ICN_PREFIX> default: /dss]")
+        sys.exit(1)
+
+    url_to_poll = sys.argv[1]
+    LOG.debug("URL to poll set to: " + url_to_poll)
+
+    try:
+        http_server_path = sys.argv[2]
+    except:
+        http_server_path = '/var/www/'
+    LOG.debug("HTTP server path set to: " + http_server_path)
+
+    try:
+        icn_prefix = sys.argv[3]
+    except:
+        icn_prefix = '/dss'
+    LOG.debug("ICN prefix set to: " + icn_prefix)
+
+    cntManager = IcnContentManager()
+    oldCntList =[]
+    while 1:
+        data = cntManager.doRequest(url_to_poll, "GET", None)
+        cntList = cntManager.generate_contentlist(data)
+        if cntList != oldCntList:
+            i = 0
+            while i < len(cntList):
+                if cntList[i] not in oldCntList:
+                    LOG.debug("Downloading file " + cntList[i])
+                    ret_code = cntManager.get_file_from_icn(cntList[i], icn_prefix, http_server_path)
+                    if ret_code == 0:
+                        i += 1
+                else:
+                    LOG.debug("File " + cntList[i] + " has been already downloaded.")
+                    i += 1
+            LOG.debug("Contentlist process complete. Next poll in 30 seconds ...")
+            oldCntList = cntList
+        else:
+            LOG.debug("No change in content list detected. Next poll in 30 seconds ...")
+        time.sleep(30)
