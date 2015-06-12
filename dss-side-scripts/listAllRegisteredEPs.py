663be9319ced7e765c9c7e8231991a4d2745d774
Mohammadjavad Valipoor <mv@softtelecom.eu>
2015-06-12 13:18:02 +0200

    - Reformat file

--- dss-side-scripts/listAllRegisteredEPs.py
+++ dss-side-scripts/listAllRegisteredEPs.py
@@ -7,41 +7,41 @@ import sys
 from keystoneclient.v2_0 import client
 
 if len(sys.argv) > 1 and sys.argv[1] == '-h':
-	print 'Usage: python listAllRegisteredEPs.py [USERNAME] [PASSWORD] [TENANT_NAME] [AUTH_URL] [SERVICE_ID]'
-	exit(0)
+    print 'Usage: python listAllRegisteredEPs.py [USERNAME] [PASSWORD] [TENANT_NAME] [AUTH_URL] [SERVICE_ID]'
+    exit(0)
 
 try:
-	user_name = sys.argv[1]
+    user_name = sys.argv[1]
 except:
-	user_name = 'dummy'
+    user_name = 'dummy'
 
 try:
-	passwd = sys.argv[2]
+    passwd = sys.argv[2]
 except:
-	passwd = 'dummy'
+    passwd = 'dummy'
 
 try:
-	tenant_Name = sys.argv[3]
+    tenant_Name = sys.argv[3]
 except:
-	tenant_Name = 'dummy'
+    tenant_Name = 'dummy'
 
 try:
-	auth_Url = sys.argv[4]
+    auth_Url = sys.argv[4]
 except:
-	auth_Url = 'http://dummy:5000/v2.0'
+    auth_Url = 'http://dummy:5000/v2.0'
 
 try:
-	s_id = sys.argv[5]
+    s_id = sys.argv[5]
 except:
-	s_id = None
+    s_id = None
 
 keystone = client.Client(username=user_name, password=passwd, tenant_name=tenant_Name, auth_url=auth_Url)
 
 ep_list = keystone.endpoints.list()
 
 if s_id is None:
-	for item in ep_list:
-		print "Service ID: \"" + item._info['service_id'] + "\" Region: \"" + item._info['region'] + "\" Public URL: \"" + item._info['publicurl'] + "\"\n"
+    for item in ep_list:
+        print "Service ID: \"" + item._info['service_id'] + "\" Region: \"" + item._info['region'] + "\" Public URL: \"" + item._info['publicurl'] + "\"\n"
 else:
     for item in ep_list:
         if item._info['service_id'] == s_id:
