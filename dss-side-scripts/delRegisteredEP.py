b65c5cb766eb58f4934e481a38988200b186d584
Mohammadjavad Valipoor <mv@softtelecom.eu>
2015-06-15 13:25:05 +0200

    - Updated list keystone registered services file - Added a script to delete registered service

--- dss-side-scripts/delRegisteredEP.py
+++ dss-side-scripts/delRegisteredEP.py
@@ -11,20 +11,20 @@ from keystoneclient.v2_0 import client
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv,"hu:p:t:a:s:",["username=","password=","tenant_name=","auth_url=","service_id="])
+        opts, args = getopt.getopt(argv,"he:u:p:t:a:",["endpoint_id=","username=","password=","tenant_name=","auth_url="])
     except getopt.GetoptError:
-        print 'Usage: python listAllRegisteredEPs.py -u [USERNAME] -p [PASSWORD] -t [TENANT_NAME] -a [AUTH_URL] -s [SERVICE_ID]'
+        print 'Usage: python listAllRegisteredEPs.py -e <ENDPOINT_ID> -u [USERNAME] -p [PASSWORD] -t [TENANT_NAME] -a [AUTH_URL]'
         exit(0)
 
     user_name = None
     passwd = None
     tenant_name = None
     auth_url = None
-    s_id = None
+    e_id = None
 
     for opt, arg in opts:
         if opt == '-h':
-            print 'Usage: python listAllRegisteredEPs.py -u [USERNAME] -p [PASSWORD] -t [TENANT_NAME] -a [AUTH_URL] -s [SERVICE_ID]'
+            print 'Usage: python listAllRegisteredEPs.py -e <ENDPOINT_ID> -u [USERNAME] -p [PASSWORD] -t [TENANT_NAME] -a [AUTH_URL]'
             exit(0)
         elif opt in ("-u", "--username"):
             user_name = arg
@@ -34,8 +34,8 @@ def main(argv):
             tenant_name = arg
         elif opt in ("-a", "--auth_url"):
             auth_url = arg
-        elif opt in ("-s", "--service_id"):
-            s_id = arg
+        elif opt in ("-e", "--endpoint_id"):
+            e_id = arg
 
     if user_name is None:
         user_name = 'dummy'
@@ -49,20 +49,21 @@ def main(argv):
     if auth_url is None:
         auth_url = 'http://dummy:5000/v2.0'
 
-    if s_id is None:
-        s_id = None
+    if e_id is None:
+        print 'Endpoint ID is mandatory!'
+        exit(0)
 
     keystone = client.Client(username=user_name, password=passwd, tenant_name=tenant_name, auth_url=auth_url)
 
     ep_list = keystone.endpoints.list()
 
-    if s_id is None:
-        for item in ep_list:
-            print "Service ID: \"" + item._info['service_id'] + "\" Region: \"" + item._info['region'] + "\" Public URL: \"" + item._info['publicurl'] + "\" Endpoint ID: \"" + item._info['id'] + "\"\n"
-    else:
-        for item in ep_list:
-            if item._info['service_id'] == s_id:
-                print "Service ID: \"" + item._info['service_id'] + "\" Region: \"" + item._info['region'] + "\" Public URL: \"" + item._info['publicurl'] + "\" Endpoint ID: \"" + item._info['id'] + "\"\n"
+    for item in ep_list:
+        if item._info['id'] == e_id:
+            res = keystone.endpoints.delete(e_id)
+            print res.__repr__()
+            return 1
+
+    print "EP not found."
 
 if __name__ == "__main__":
     main(sys.argv[1:])
