34023cd6111358f08d20491a21928c4d4e6d4daa
Mohammadjavad Valipoor <mv@softtelecom.eu>
2015-10-22 13:48:18 +0200

    -

--- dss-side-scripts/icn_dss_dns_load_test.py
+++ dss-side-scripts/icn_dss_dns_load_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import getopt
+import threading
 
 __author__ = "Mohammad Valipoor"
 __copyright__ = "Copyright 2014, SoftTelecom"
@@ -75,6 +76,78 @@ class IcnContentManager:
             ret_code = call(['rm', http_server_path + filename])
         return ret_code
 
+class icnThread(threading.Thread):
+    def __init__(self, url_to_poll, icn_api_url, request_delay, http_server_path, icn_prefix, ready_event):
+        self.event = ready_event
+        self.testComponent = 'icnThread'
+        threading.Thread.__init__(self)
+        print self.testComponent + " initialized."
+
+        self.url_to_poll = url_to_poll
+        self.icn_api_url = icn_api_url
+        self.request_delay = request_delay
+        self.http_server_path = http_server_path
+        self.icn_prefix = icn_prefix
+
+    def run(self):
+        print self.testComponent + " started."
+        cntManager = IcnContentManager()
+        oldRouterList = {"routers":[]}
+        while 1:
+            resp_routers = cntManager.doRequest(self.icn_api_url + '/icnaas/api/v1.0/endpoints/client','GET','')
+
+            if oldRouterList != resp_routers:
+                i = 0
+                while i < len(resp_routers["routers"]):
+                    if resp_routers["routers"][i] not in oldRouterList["routers"]:
+                        ret_code = call(['/home/ubuntu/ccnxdir/bin/ccndc', 'add', 'ccnx:' + self.icn_prefix, 'tcp', resp_routers["routers"][i]["public_ip"], '9695'])
+                        time.sleep(0.2)
+                        ret_code = call(['/home/ubuntu/ccnxdir/bin/ccndc', 'add', 'ccnx:/ccnx.org', 'tcp', resp_routers["routers"][i]["public_ip"], '9695'])
+                        time.sleep(0.2)
+                    i += 1
+
+                oldRouterList = resp_routers
+
+                ret_code = call(['/home/ubuntu/ccnxdir/bin/ccndc', 'setstrategy', 'ccnx:' + self.icn_prefix, 'loadsharing'])
+
+            data = cntManager.doCurlRequest(self.url_to_poll)
+            cntList = cntManager.generate_contentlist(data)
+            i = 0
+            while i < len(cntList):
+                ret_code = cntManager.get_file_from_icn(cntList[i], self.icn_prefix, self.http_server_path)
+                if ret_code == 0:
+                    i += 1
+                else:
+                    print "Error while getting content " + str(i)
+                time.sleep(self.request_delay)
+            i = 0
+            while i < len(cntList):
+                ret_code = cntManager.remove_file(cntList[i], self.http_server_path)
+                if ret_code == 0:
+                    i += 1
+                else:
+                    print "Error while removing content " + str(i)
+                time.sleep(0.1)
+
+class playerThread(threading.Thread):
+    def __init__(self, url_to_poll, ready_event):
+        self.event = ready_event
+        self.testComponent = 'playerThread'
+        threading.Thread.__init__(self)
+        print self.testComponent + " initialized."
+
+        parseUrl = url_to_poll.split("/")
+        self.url_to_poll = "http://" + parseUrl[2] + "/WebAppDSS/display/playAll?" + parseUrl[5].split("?")[1] + "&type_request=refresh"
+
+    def run(self):
+        print self.testComponent + " started."
+        print self.url_to_poll
+        cntManager = IcnContentManager()
+        while 1:
+            data = cntManager.doCurlRequest(self.url_to_poll)
+            time.sleep(0.2)
+
+
 def main(argv):
     try:
         opts, args = getopt.getopt(argv,"hu:i:t:f:p:",["url=","icn=","time=","file_path=","prefix="])
@@ -107,6 +180,10 @@ def main(argv):
         print 'Polling URL is mandatory!'
         exit(0)
 
+    if icn_api_url is None:
+        print 'ICN Polling URL is mandatory!'
+        exit(0)
+
     if request_delay is None:
         request_delay = 0.5
 
@@ -116,43 +193,12 @@ def main(argv):
     if icn_prefix is None:
         icn_prefix = '/dss'
 
-    cntManager = IcnContentManager()
-    oldRouterList = {"routers":[]}
-    while 1:
-        resp_routers = cntManager.doRequest(icn_api_url + '/icnaas/api/v1.0/endpoints/client','GET','')
 
-        if oldRouterList != resp_routers:
-            i = 0
-            while i < len(resp_routers["routers"]):
-                if resp_routers["routers"][i] not in oldRouterList["routers"]:
-                    ret_code = call(['/home/ubuntu/ccnxdir/bin/ccndc', 'add', 'ccnx:' + icn_prefix, 'tcp', resp_routers["routers"][i]["public_ip"], '9695'])
-                    time.sleep(0.2)
-                    ret_code = call(['/home/ubuntu/ccnxdir/bin/ccndc', 'add', 'ccnx:/ccnx.org', 'tcp', resp_routers["routers"][i]["public_ip"], '9695'])
-                    time.sleep(0.2)
-                i += 1
-
-            oldRouterList = resp_routers
-
-            ret_code = call(['/home/ubuntu/ccnxdir/bin/ccndc', 'setstrategy', 'ccnx:' + icn_prefix, 'loadsharing'])
-
-        data = cntManager.doCurlRequest(url_to_poll)
-        cntList = cntManager.generate_contentlist(data)
-        i = 0
-        while i < len(cntList):
-            ret_code = cntManager.get_file_from_icn(cntList[i], icn_prefix, http_server_path)
-            if ret_code == 0:
-                i += 1
-            else:
-                print "Error while getting content " + str(i)
-            time.sleep(request_delay)
-        i = 0
-        while i < len(cntList):
-            ret_code = cntManager.remove_file(cntList[i], http_server_path)
-            if ret_code == 0:
-                i += 1
-            else:
-                print "Error while removing content " + str(i)
-            time.sleep(0.1)
+    shared_event = threading.Event()
+    icn_thread = icnThread(url_to_poll, icn_api_url, request_delay, http_server_path, icn_prefix, shared_event)
+    player_thread = playerThread(url_to_poll, shared_event)
+    icn_thread.start()
+    player_thread.start()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
