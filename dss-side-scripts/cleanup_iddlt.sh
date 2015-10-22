0744ca168d9224dce182ee36294575ee357e98e9
Mohammadjavad Valipoor <mv@softtelecom.eu>
2015-10-22 18:49:36 +0200

    - adding the bang :-/

--- dss-side-scripts/cleanup_iddlt.sh
+++ dss-side-scripts/cleanup_iddlt.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 kill `ps aux | grep icn_dss_dns_load_test.py | grep -v grep | awk '{print $2}'`
 mv /etc/resolvconf/resolv.conf.d/head.bak /etc/resolvconf/resolv.conf.d/head
 mv /etc/resolv.conf.bak /etc/resolv.conf
