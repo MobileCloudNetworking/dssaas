579d44b190e69962fc5ef1234341ac76354e6f3f
Mohammadjavad Valipoor <mv@softtelecom.eu>
2015-10-22 15:45:00 +0200

    -

--- dss-side-scripts/run_iddlt.sh
+++ dss-side-scripts/run_iddlt.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-if [ "$#" -ne 4 ]; then
+if [ "$#" -ne 5 ]; then
     echo "Check number of parameters"
     exit 1
 fi
 
-icn_api="$1"
-player_id="$2"
-timeout="$3"
-dns_forwarder="$4"
+execution_count="$1"
+icn_api="$2"
+player_id="$3"
+timeout="$4"
+dns_forwarder="$5"
 
 echo "nameserver $dns_forwarder" >> /etc/resolvconf/resolv.conf.d/head
 resolvconf -u
@@ -16,7 +17,7 @@ export CCND_CAP=0
 sleep 10
 
 i=1
-while [ $i -le 10 ]
+while [ $i -le $(eval echo $execution_count) ]
 do
     mkdir -p /home/ubuntu/test/file_$i
     nohup python /home/ubuntu/icn_dss_dns_load_test.py -u http://dashboard.dssaas.mcn.com:8080/WebAppDSS/display/listContents?id=$player_id -i $icn_api -t $timeout -f ./test/file_$i &
