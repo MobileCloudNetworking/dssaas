dffbad6952813e3b73b5640d84e0bf108e8cb543
Mohammadjavad Valipoor <mv@softtelecom.eu>
2015-12-30 13:37:05 +0100

    -

--- dss-file-sync/main.py
+++ dss-file-sync/main.py
@@ -17,14 +17,8 @@ import signal
 import sys
 import time
 
-def clean_up(broadcast_manager, torrent_manager):
-    print "Exiting threads gracefully ...\n"
-    broadcast_manager.terminated = True
-    torrent_manager.terminated = True
-    print "Bye.\n"
-    sys.exit(0)
 
-if __name__ == "__main__":
+def main_func():
     conf = Config()
     logger = Log.config_logger(conf.get('log', 'name'))
     logger.debug('main message')
@@ -37,6 +31,13 @@ if __name__ == "__main__":
     torrent_manager = TorrentManager(file_manager, session_manager, tracker_manager)
     broadcast_manager = BroadcastManager(file_manager, torrent_manager, tracker_manager)
 
+    def clean_up(signal, frame):
+        print "Exiting threads gracefully ...\n"
+        broadcast_manager.terminated = True
+        torrent_manager.terminated = True
+        print "Bye.\n"
+        sys.exit(0)
+
     #running threads
     #broadcast_manager.sendAmazon_broadcast_message()
     broadcast_manager.send_broadcast_message()
@@ -46,11 +47,13 @@ if __name__ == "__main__":
     torrent_manager.check_files_status()
     torrent_manager.cleanup_deleted_files()
 
-    signal.signal(signal.SIGINT, clean_up(broadcast_manager, torrent_manager))
-    signal.pause()
+    signal.signal(signal.SIGINT, clean_up)
 
     for i in range(1, 5000):
         #print "Sleeping...\n"
         time.sleep(1)
         for torrent_file in file_manager.list_files(path, ['.torrent'])[1]:
             session_manager.get_torrent_stat_str(torrent_file.split('.')[0] + '.webm')
+
+if __name__ == "__main__":
+    main_func()
