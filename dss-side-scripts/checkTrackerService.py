555d796ac86d6bb6dbd550a1d4c73096fd08314f
Mj Valipoor <mv@softtelecom.eu>
2016-04-01 13:04:16 +0200

    - Bug fix

--- dss-side-scripts/checkTrackerService.py
+++ dss-side-scripts/checkTrackerService.py
@@ -9,15 +9,13 @@ import os
 
 process_id = None
 process_name = "opentracker"
-# Read the file on /var/run/proc_name.pid and fetch the pid
-file_object = open("/var/run/" + process_name + ".pid", "r")
-process_id = file_object.read().strip()
-file_object.close()
-
 try:
-        os.kill(int(process_id), 0)
-        print "1"
+    # Read the file on /var/run/proc_name.pid and fetch the pid
+    file_object = open("/var/run/opentracker/" + process_name + ".pid", "r")
+    process_id = file_object.read().strip()
+    file_object.close()
+    os.kill(int(process_id), 0)
+    print "1"
 except Exception as e:
-        print "0"
-
+    print "0"
 exit(0)
